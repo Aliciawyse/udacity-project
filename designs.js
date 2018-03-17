ec02c42272daf5986ce9fd888f199a73ee88f3d2
Barrett <Aliciawyse@users.noreply.github.com>
2018-03-16 22:14:49 -0400

    wrote function to grab values in input fields on submit

--- designs.js
+++ designs.js
@@ -7,4 +7,26 @@ function makeGrid() {
 
 // Your code goes here!
 
+
 }
+
+
+//on submit
+//grab values and make a grid
+
+function grabValues() {
+
+    $("#sizePicker").submit(function(event){
+
+        var gridHeight = $("#inputHeight").val();
+        var gridWidth = $("#inputWidth").val();
+
+        // console.log($("#inputHeight").val());
+        // console.log($("#inputWidth").val());
+    })
+
+    //call make grid()
+
+}
+
+grabValues();
